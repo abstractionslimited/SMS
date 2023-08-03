bd68695a3174ff4002c1d42fbdca627d632b2eb9
Khanya <sibulelekupelo@gmail.com>
2023-08-03 06:37:35 +0200

    Create period render

--- apps/timetable/app/config.js
+++ apps/timetable/app/config.js
@@ -10,6 +10,18 @@ define(function () {
           'Friday',
           'Saturday',
           'Sunday'
+        ],
+        periods: [
+          { start: '07:50', end: '08:35' },
+          { start: '08:40', end: '09:25' },
+          { start: '09:40', end: '10:25' },
+          { start: '10:30', end: '11:15' },
+          { start: '11:30', end: '12:15' },
+          { start: '12:15', end: '13:00' },
+          { start: '13:15', end: '14:00' },
+          { start: '14:00', end: '14:45' },
+          { start: '14:45', end: '15:30' },
+          { start: '15:30', end: '16:15' }
         ]
       };
     }
--- apps/timetable/app/main.js
+++ apps/timetable/app/main.js
@@ -26,7 +26,7 @@ define(function (require) {
           button.classList.remove('open');
         }
 
-        e.target.classList.add('open');
+        // e.target.classList.add('open');
         timetable.showDayView(e.target.getAttribute('day-dayNumber'));
       });
 
@@ -35,8 +35,54 @@ define(function (require) {
     }
   }
 
-  createHeaderButtons();
-  console.log('timetable rendered ..');
+  function renderPeriodsContainer(params) {
+    var i = 0;
+
+    for (const period of configs.periods) {
+      let container = document.createElement('div');
+      let periodElement = document.createElement('div');
+      let contentElement = document.createElement('div');
+
+      container.setAttribute('class', 'routine-schedule');
+      container.classList.add('hidden');
+      container.style.transitionDelay = i * 0.025 + 's';
+
+      periodElement.setAttribute('class', 'period-element');
+      contentElement.setAttribute('class', 'period-content');
+
+      var start = period.start.split(':');
+      var startAMP = start[0] >= 12 ? 'PM' : 'AM';
+      var startHour = start[0] >= 13 ? parseInt(start[0]) - 12 : start[0];
+      var startMinutes = start[1];
+
+      var end = period.end.split(':');
+      var endAMP = end[0] >= 12 ? 'PM' : 'AM';
+      var endHour = end[0] >= 13 ? parseInt(end[0]) - 12 : end[0];
+      var endMinutes = end[1];
+
+      startHour = String(startHour).padStart(2, 0);
+      startMinutes = String(startHour).padStart(2, 0);
+
+      endHour = String(endHour).padStart(2, 0);
+      endMinutes = String(endMinutes).padStart(2, 0);
+
+      periodElement.innerHTML = `${startHour}: ${startMinutes} ${startAMP} - ${endHour}:${endMinutes} ${endAMP}`;
+      contentElement.innerHTML = '<div></div><div></div>';
+
+      container.innerHTML += periodElement.outerHTML;
+      container.innerHTML += contentElement.outerHTML;
+
+      document.getElementById('timetable').appendChild(container);
+      i++;
+    }
+    console.log('rendered periods...');
+  }
+
+  (function renderTimetable(params) {
+    createHeaderButtons();
+    renderPeriodsContainer();
+    console.log('timetable rendered..');
+  })();
 });
 
 window.onload = function () {
